dfe26da51e6de7111696acf4e7ba3a4e31b59fe7
Juan S Orozco Buitrago <Sebasorozcob@gmail.com>
2022-03-14 19:44:32 -0400

    Adding the logo and different colors set

---
+++ SC Groceries/App/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  SC Groceries
+//
+//  Created by Juan Sebastian Orozco Buitrago on 3/14/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HomeView()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
--- SC Groceries/View/HomeView.swift
+++ SC Groceries/View/HomeView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  HomeView.swift
 //  SC Groceries
 //
 //  Created by Juan Sebastian Orozco Buitrago on 3/14/22.
@@ -7,15 +7,14 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct HomeView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        HomeView()
     }
 }
