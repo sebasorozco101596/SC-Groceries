6fb1517665396ca6a99c81405fd80847bf813779
Juan S Orozco Buitrago <Sebasorozcob@gmail.com>
2022-03-14 19:08:05 -0400

    Initial Commit

---
+++ SC Groceries/SC_GroceriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SC_GroceriesApp.swift
+//  SC Groceries
+//
+//  Created by Juan Sebastian Orozco Buitrago on 3/14/22.
+//
+
+import SwiftUI
+
+@main
+struct SC_GroceriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
